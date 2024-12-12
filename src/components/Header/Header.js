c57051d5e0e668a7e5f6abd0067ae15a3773d240
Caroline Vieira <carolinesanvieira@gmail.com>
2024-12-12 19:28:26 +0100

    theming with cSS variables

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, QUERIES, WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from '../../constants';
 import Icon from '../Icon';
 import Logo from '../Logo';
 import MobileMenu from '../MobileMenu';
@@ -63,8 +63,8 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  border-bottom: 1px solid ${COLORS.gray[300]};
-  border-top:4px solid ${COLORS.gray[900]} ;
+  border-bottom: 1px solid var(--color-gray-300);
+  border-top:4px solid var(--color-gray-900) ;
   overflow: auto;
 
   @media ${QUERIES.tabletAndSmaller} {
@@ -127,11 +127,11 @@ const NavLink = styled.a`
   font-size: 1.125rem;
   text-transform: uppercase;
   text-decoration: none;
-  color: ${COLORS.gray[900]};
+  color: var(--color-gray-900);
   font-weight: ${WEIGHTS.medium};
 
   &:first-of-type {
-    color: ${COLORS.secondary};
+    color: var(--color-secondary);
   }
 `;
 
